24510f1c3be8a0becee742a986dc24aa4a880c20
Lude Ribeiro <ludeyuri07@gmail.com>
2025-09-01 21:12:43 -0300

    feat: Atualizar Configuração de URLs para API v1

--- project/core/api/urls/base_urls.py
+++ project/core/api/urls/base_urls.py
@@ -1,25 +1,7 @@
-from core.api.v1.views import user
 from django.urls import include, path
 
 app_name = "core"
 
-user = [
-    path(
-        "user/list",
-        user.UserListAPIView.as_view(),
-        name="user_list",
-    ),
-    path(
-        "user/alter_password/<int:pk>/",
-        user.UserAlterPasswordAPIView.as_view(),
-        name="user_alter_password",
-    ),
-    path(
-        "user/register",
-        user.UserCreateAPIView.as_view(),
-        name="user_register",
-    ),
-]
 
 urlpatterns = [
     path(
@@ -27,4 +9,4 @@ urlpatterns = [
         include("core.api.urls.v1_urls"),
         name="v1",
     ),
-] + user
+]
--- project/core/api/urls/v1_urls.py
+++ project/core/api/urls/v1_urls.py
@@ -1,4 +1,18 @@
-from core.api.v1.views import user
+from core.api.v1.views import auth, user
 from django.urls import include, path
 
-urlpatterns: list[str] = []
+urlpatterns = [
+    path("users/", user.UserCreateAPIView.as_view(), name="create-user"),
+    path("users/list/", user.UserListAPIView.as_view(), name="user-list"),
+    path(
+        "users/alter_password/<uuid:pk>/",
+        user.UserAlterPasswordAPIView.as_view(),
+        name="user-alter-password",
+    ),
+    path(
+        "users/<uuid:pk>/",
+        user.UserRetrieveAPIView.as_view(),
+        name="retrieve-user",
+    ),
+    path("login/", auth.LoginAPIView.as_view(), name="login"),
+]
