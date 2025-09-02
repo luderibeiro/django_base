b666552859198177f59c6598a3f17ed81e85d05f
Lude Ribeiro <ludeyuri07@gmail.com>
2025-09-01 21:32:52 -0300

    refactor: Ajustar formatação e imports em auth_gateway_impl.py

--- project/core/repositories/auth_gateway_impl.py
+++ project/core/repositories/auth_gateway_impl.py
@@ -1,9 +1,9 @@
-from typing import Tuple
 import os
+from typing import Tuple
 
 from core.domain.gateways import AuthGateway
 from django.contrib.auth import authenticate, get_user_model
-from oauth2_provider.models import AccessToken, RefreshToken, Application
+from oauth2_provider.models import AccessToken, Application, RefreshToken
 
 User = get_user_model()
 
@@ -28,8 +28,12 @@ class DjangoAuthGateway(AuthGateway):
             application = Application.objects.get(name="Default Application")
         except Application.DoesNotExist:
             # Se não existir, crie uma (apenas para ambiente de desenvolvimento/teste)
-            application = Application.objects.create(name="Default Application", client_type="public", authorization_grant_type="password")
-        
+            application = Application.objects.create(
+                name="Default Application",
+                client_type="public",
+                authorization_grant_type="password",
+            )
+
         # Remove quaisquer tokens existentes para o usuário e aplicação
         AccessToken.objects.filter(user=user, application=application).delete()
         RefreshToken.objects.filter(user=user, application=application).delete()
@@ -38,7 +42,10 @@ class DjangoAuthGateway(AuthGateway):
         access_token = AccessToken.objects.create(
             user=user,
             application=application,
-            token="access_token_" + str(user_id) + "_" + os.urandom(30).hex(), # Gerar um token real
+            token="access_token_"
+            + str(user_id)
+            + "_"
+            + os.urandom(30).hex(),  # Gerar um token real
             scope="read write",
             expires=AccessToken.get_expiration_delta(),
         )
@@ -47,7 +54,10 @@ class DjangoAuthGateway(AuthGateway):
         refresh_token = RefreshToken.objects.create(
             user=user,
             application=application,
-            token="refresh_token_" + str(user_id) + "_" + os.urandom(30).hex(), # Gerar um token real
+            token="refresh_token_"
+            + str(user_id)
+            + "_"
+            + os.urandom(30).hex(),  # Gerar um token real
             access_token=access_token,
         )
 
